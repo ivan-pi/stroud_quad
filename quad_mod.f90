88e6863f174af7c5b0c35cbd731bbd14bd1dd319
Ivan Pribec <ivan.pribec@gmail.com>
2019-11-03 20:03:04 +0100

    Started refactoring Jacobi routine

--- quad_mod.f90
+++ quad_mod.f90
@@ -12,135 +12,177 @@ module quad_mod
 
 contains
 
-!     SUBROUTINE jacobi(nn,x,a,alf,bta,b,c,eps,csx,csa,tsx,tsa)
+    !                  CALCULATES THE ZEROS X(I) OF THE NN-TH ORDER
+    !                JACOBI POLYNOMIAL PN(ALF,BTA) FOR THE SEGMENT (-1,1).
+    !                THE LARGEST ZERO WILL BE STORED IN X(l). ALSO
+    !                CALCULATES THE CORRESPONDING COEFFICIENTS  A(I)
+    !                OF THE NN-TH ORDER GAUSS-JACOBI QUADRATURE FORMULA
+    !                OF DEGREE 2*NN-1.
+    !                  THIS SUBROUTINE MUST BE GIVEN THE COEFFICIENTS
 
-!     !                  CALCULATES THE ZEROS X(I) OF THE NN-TH ORDER
-!     !                JACOBI POLYNOMIAL PN(ALF,BTA) FOR THE SEGMENT (-1,1).
-!     !                THE LARGEST ZERO WILL BE STORED IN X(l). ALSO
-!     !                CALCULATES THE CORRESPONDING COEFFICIENTS  A(I)
-!     !                OF THE NN-TH ORDER GAUSS-JACOBI QUADRATURE FORMULA
-!     !                OF DEGREE 2*NN-1.
-!     !                  THIS SUBROUTINE MUST BE GIVEN THE COEFFICIENTS
+    !                                  (ALF+BTA)(BTA-ALF)
+    !                      B(N) =  --------------------------
+    !                              (ALF+BTA+2N)(ALF+BTA+2N-2)
 
-!     !                                  (ALF+BTA)(BTA-ALF)
-!     !                      B(N) =  --------------------------
-!     !                              (ALF+BTA+2N)(ALF+BTA+2N-2)
+    !                            4(N-1MALF+N-1) ( BTA+N-1) (ALF+BTA+N-1)
+    !                C(N) =  ---------------------------------------------
+    !                        (ALF+BTA+2N-1)(ALF+BTA+2N-2)**2(ALF+BTA+2N-3)
 
-!     !                            4(N-1MALF+N-1) ( BTA+N-1) (ALF+BTA+N-1)
-!     !                C(N) =  ---------------------------------------------
-!     !                        (ALF+BTA+2N-1)(ALF+BTA+2N-2)**2(ALF+BTA+2N-3)
+    !                IN THE RECURSION RELATION
 
-!     !                IN THE RECURSION RELATION
+    !                      P(N) = (X - B(N))*P(N-1) - C(N)*P(N-2)
 
-!     !                      P(N) = (X - B(N))*P(N-1) - C(N)*P(N-2)
+    !                FOR ALL N LESS THAN OR EQUAL TO THE HIGHEST DEGREE NN.
 
-!     !                FOR ALL N LESS THAN OR EQUAL TO THE HIGHEST DEGREE NN.
+    !                    CSX = CALC SUM X(I)    TSX = TRUE SUM X(I)
+    !                    CSA = CALC SUM A(I)    TSA = TRUE SUM A(I)
+    subroutine jacobi(nn,x,a,alf,bta,b,c,eps,csx,csa,tsx,tsa)
+        integer, intent(in) :: nn
+        real(wp), intent(out) :: x(50)
+        real(wp), intent(out) :: a(50)
+        real(wp), intent(in) :: alf
+        real(wp), intent(in) :: bta
+        real(wp), intent(in) :: b(50)
+        real(wp), intent(in) :: c(50)
+        real(wp), intent(in) :: eps
+        real(wp), intent(out) :: csx
+        real(wp), intent(out) :: csa
+        real(wp), intent(out) :: tsx
+        real(wp), intent(out) :: tsa
 
-!     !                    CSX = CALC SUM X(I)    TSX = TRUE SUM X(I)
-!     !                    CSA = CALC SUM A(I)    TSA = TRUE SUM A(I)
+        integer :: i
+        real(wp) :: fn, beta, cc
+        real(wp) :: an, bn, r1, r2, r3, ratio, xt
 
+        fn = nn
+        csx = 0.0_wp
+        csa = 0.0_wp
+        beta = exp(log_gamma(alf+1.0_wp) + log_gamma(bta+1.0_wp) - log_gamma(alf+bta+2.0_wp))
+        cc = 2.0_wp**(alf+bta+1.0_wp)*beta
+        tsx = fn*(bta - alf)/(alf + bta + 2.0_wp*fn)
+        tsa = cc
+        do  j=2,nn
+          cc = cc*c(j)
+        end do
+    do i = 1, nn
 
+        select case(i)
+        case(1)                  ! largest zero
+            an = alf/fn
+            bn = bta/fn
+            r1 = (1. + alf)*(2.78/(4. + fn*fn) + .768*an/fn)
+            r2 = 1. + 1.48*an + .96*bn + .452*an*an + .83*an*bn
+            xt = 1. - r1/r2 
+        case(2)                  ! second zero
+            r1 = (4.1+alf)/((1.+alf)*(1.+.156*alf))
+            r2 = 1. + .06*(fn-8.)*(1.+.12*alf)/fn
+            r3 = 1. + .012*bta*(1.+.25*ABS(alf))/fn
+            ratio = r1*r2*r3
+            xt = xt - ratio*(1.-xt)
+        case(3)
+            r1 = (1.67+.28*alf)/(1.+.37*alf)
+            r2 = 1. + .22*(fn-8.)/fn
+            r3 = 1. + 8.*bta/((6.28+bta)*fn*fn)
+            ratio = r1*r2*r3
+            xt = xt - ratio*(x(1) - xt)
+        case(nn-1) ! second last zero
+            r1 = (1. + .235*bta)/(.766+.119*bta)
+            r2 = 1./( 1. + .639*(fn-4.)/(1.+.71*(fn-4.)) )
+            r3 = 1./( 1. + 20.*alf/((7.5+alf)*fn*fn) )
+            ratio = r1*r2*r3
+            xt = xt + ratio*(xt-x(i-2))
+        case(nn) ! last zero
+            r1 = (1.+.37*bta)/(1.67+.28*bta)
+            r2 = 1./( 1. + .22*(fn-8.)/fn )
+            r3 = 1./( 1. + 8.*alf/((6.28+alf)*fn*fn) )
+            ratio = r1*r2*r3
+            xt = xt + ratio*(xt-x(i-2))
+        case default !middle zeros
+            xt = 3.*x(i-1) - 3.*x(i-2) + x(i-3)
+        end select
+        call root(xt,nn,alf,bta,dpn,pn1,b,c,eps)
+        x(i) = xt
+        a(i) = cc/(dpn*pn1)
+        WRITE(*,20) alf,bta,nn,i,xt,a(i)
+        csx = csx + xt
+        csa = csa + a(i)
 
-!     INTEGER, INTENT(IN)                      :: nn
-!     REAL, INTENT(IN OUT)                     :: x(50)
-!     REAL, INTENT(OUT)                        :: a(50)
-!     REAL, INTENT(IN)                         :: alf
-!     REAL, INTENT(IN)                         :: bta
-!     REAL, INTENT(IN OUT)                     :: b(50)
-!     REAL, INTENT(IN)                         :: c(50)
-!     REAL, INTENT(IN OUT)                     :: eps
-!     REAL, INTENT(OUT)                        :: csx
-!     REAL, INTENT(OUT)                        :: csa
-!     REAL, INTENT(OUT)                        :: tsx
-!     REAL, INTENT(OUT)                        :: tsa
-
-!     fn = nn
-!     csx = 0.0_wp
-!     csa = 0.0_wp
-!     beta = exp(log_gamma(alf+1.0_wp) + log_gamma(bta+1.0_wp) - log_gamma(alf+bta+2.0_wp))
-!     cc = 2.0_wp**(alf+bta+1.0_wp)*beta
-!     tsx = fn*(bta - alf)/(alf + bta + 2.0_wp*fn)
-!     tsa = cc
-!     do  j=2,nn
-!       cc = cc*c(j)
-!     end do
-!     DO i = 1, nn
-!       IF( i-1  < 0) THEN
-!         GO TO    12
-!       ELSE IF ( i-1  == 0) THEN
-!         GO TO     2
-!       ELSE
-!         GO TO     3
-!       END IF
-!     !                  LARGEST ZERO
-!       2 an = alf/fn
-!       bn = bta/fn
-!       r1 = (1.+alf)*(2.78/(4.+fn*fn) + .768*an/fn)
-!       r2 = 1. + 1.48*an + .96*bn + .452*an*an + .83*an*bn
-!       xt = 1. - r1/r2
-!       GO TO 11
-!       3 IF( i-2  < 0) THEN
-!         GO TO    12
-!       ELSE IF ( i-2  == 0) THEN
-!         GO TO     4
-!       ELSE
-!         GO TO     5
-!       END IF
-!     !                  SECOND ZERO
-!       4 r1 = (4.1+alf)/((1.+alf)*(1.+.156*alf))
-!       r2 = 1. + .06*(fn-8.)*(1.+.12*alf)/fn
-!       r3 = 1. + .012*bta*(1.+.25*ABS(alf))/fn
-!       ratio = r1*r2*r3
-!       xt = xt - ratio*(1.-xt)
-!       GO TO 11
-!       5 IF( i-3  < 0) THEN
-!         GO TO    12
-!       ELSE IF ( i-3  == 0) THEN
-!         GO TO     6
-!       ELSE
-!         GO TO     7
-!       END IF
-!     !                  THIRD ZERO
-!       6 r1 = (1.67+.28*alf)/(1.+.37*alf)
-!       r2 = 1. + .22*(fn-8.)/fn
-!       r3 = 1. + 8.*bta/((6.28+bta)*fn*fn)
-!       ratio = r1*r2*r3
-!       xt = xt - ratio*(x(1) - xt)
-!       GO TO 11
-!       7 IF(nn-i-1 < 0) THEN
-!         GO TO    10
-!       ELSE IF (nn-i-1 == 0) THEN
-!         GO TO     9
-!       END IF
-!     !                  MIDDLE ZEROS
-!       8 xt = 3.*x(i-1) - 3.*x(i-2) + x(i-3)
-!       GO TO 11
-!     !                  SECOND LAST ZERO
-!       9 r1 = (1. + .235*bta)/(.766+.119*bta)
-!       r2 = 1./( 1. + .639*(fn-4.)/(1.+.71*(fn-4.)) )
-!       r3 = 1./( 1. + 20.*alf/((7.5+alf)*fn*fn) )
-!       ratio = r1*r2*r3
-!       xt = xt + ratio*(xt-x(i-2))
-!       GO TO 11
-!     !                  LAST ZERO
-!       10 r1 = (1.+.37*bta)/(1.67+.28*bta)
-!       r2 = 1./( 1. + .22*(fn-8.)/fn )
-!       r3 = 1./( 1. + 8.*alf/((6.28+alf)*fn*fn) )
-!       ratio = r1*r2*r3
-!       xt = xt + ratio*(xt-x(i-2))
+      IF( i-1  < 0) THEN
+        GO TO    12
+      ELSE IF ( i-1  == 0) THEN
+        GO TO     2
+      ELSE
+        GO TO     3
+      END IF
+    !                  LARGEST ZERO
+      2 an = alf/fn
+      bn = bta/fn
+      r1 = (1.+alf)*(2.78/(4.+fn*fn) + .768*an/fn)
+      r2 = 1. + 1.48*an + .96*bn + .452*an*an + .83*an*bn
+      xt = 1. - r1/r2
+      GO TO 11
+      3 IF( i-2  < 0) THEN
+        GO TO    12
+      ELSE IF ( i-2  == 0) THEN
+        GO TO     4
+      ELSE
+        GO TO     5
+      END IF
+    !                  SECOND ZERO
+      4 r1 = (4.1+alf)/((1.+alf)*(1.+.156*alf))
+      r2 = 1. + .06*(fn-8.)*(1.+.12*alf)/fn
+      r3 = 1. + .012*bta*(1.+.25*ABS(alf))/fn
+      ratio = r1*r2*r3
+      xt = xt - ratio*(1.-xt)
+      GO TO 11
+      5 IF( i-3  < 0) THEN
+        GO TO    12
+      ELSE IF ( i-3  == 0) THEN
+        GO TO     6
+      ELSE
+        GO TO     7
+      END IF
+    !                  THIRD ZERO
+      6 r1 = (1.67+.28*alf)/(1.+.37*alf)
+      r2 = 1. + .22*(fn-8.)/fn
+      r3 = 1. + 8.*bta/((6.28+bta)*fn*fn)
+      ratio = r1*r2*r3
+      xt = xt - ratio*(x(1) - xt)
+      GO TO 11
+      7 IF(nn-i-1 < 0) THEN
+        GO TO    10
+      ELSE IF (nn-i-1 == 0) THEN
+        GO TO     9
+      END IF
+    !                  MIDDLE ZEROS
+      8 xt = 3.*x(i-1) - 3.*x(i-2) + x(i-3)
+      GO TO 11
+    !                  SECOND LAST ZERO
+      9 r1 = (1. + .235*bta)/(.766+.119*bta)
+      r2 = 1./( 1. + .639*(fn-4.)/(1.+.71*(fn-4.)) )
+      r3 = 1./( 1. + 20.*alf/((7.5+alf)*fn*fn) )
+      ratio = r1*r2*r3
+      xt = xt + ratio*(xt-x(i-2))
+      GO TO 11
+    !                  LAST ZERO
+      10 r1 = (1.+.37*bta)/(1.67+.28*bta)
+      r2 = 1./( 1. + .22*(fn-8.)/fn )
+      r3 = 1./( 1. + 8.*alf/((6.28+alf)*fn*fn) )
+      ratio = r1*r2*r3
+      xt = xt + ratio*(xt-x(i-2))
       
-!       11 CALL root(xt,nn,alf,bta,dpn,pn1,b,c,eps)
-!         x(i) = xt
-!         a(i) = cc/(dpn*pn1)
-!         WRITE(*,20) alf,bta,nn,i,xt,a(i)
-!         csx = csx + xt
-!         csa = csa + a(i)
-!     end do
+      11 CALL root(xt,nn,alf,bta,dpn,pn1,b,c,eps)
+        x(i) = xt
+        a(i) = cc/(dpn*pn1)
+        WRITE(*,20) alf,bta,nn,i,xt,a(i)
+        csx = csx + xt
+        csa = csa + a(i)
+    end do
 
-!     write(*,20) alf,bta,nn,i,csx,csa,tsx,tsa
-! 20  format(2f6.2,2i3,2(1x,e14.8),2x,2(1x,e14.8))
+    write(*,20) alf,bta,nn,i,csx,csa,tsx,tsa
+20  format(2f6.2,2i3,2(1x,e14.8),2x,2(1x,e14.8))
 
-!     end subroutine
+    end subroutine
 
     !                  IMPROVES THE APPROXIMATE ROOT X
     !                IN ADDITION WE ALSO OBTAIN
@@ -184,10 +226,10 @@ contains
         integer :: j
         real(wp) :: p1, p, dp1, dp, q, dq
 
-        p1  = 1
-        p   = x + (alf-bta)/(alf+bta+2.)
-        dp1 = 0
-        dp  = 1
+        p1  = 1.0_wp
+        p   = x + (alf-bta)/(alf+bta+2.0_wp)
+        dp1 = 0.0_wp
+        dp  = 1.0_wp
         do j = 2, nn
             q = (x - b(j))*p - c(j)*p1
             dq = (x - b(j))*dp + p - c(j)*dp1
